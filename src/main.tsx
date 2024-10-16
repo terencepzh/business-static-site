2a701dd804f89b6f07e63102d9e80a1029ca2f62
lennon <lemoncode8888@gmail.com>
2024-10-16 19:02:36 +0800

    removed basename

--- src/main.tsx
+++ src/main.tsx
@@ -6,27 +6,24 @@ import Home from "./pages/Home.tsx";
 import Contact from "./pages/Contact.tsx";
 import Layout from "./layouts/Layout.tsx";
 
-const router = createBrowserRouter(
-  [
-    {
-      path: "/",
-      element: (
-        <Layout>
-          <Home />
-        </Layout>
-      ),
-    },
-    {
-      path: "/contact-us",
-      element: (
-        <Layout>
-          <Contact />
-        </Layout>
-      ),
-    },
-  ],
-  { basename: "/business-static-site/" } // for development only
-);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <Layout>
+        <Home />
+      </Layout>
+    ),
+  },
+  {
+    path: "/contact-us",
+    element: (
+      <Layout>
+        <Contact />
+      </Layout>
+    ),
+  },
+]);
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,6 @@ import react from "@vitejs/plugin-react-swc";
 import { defineConfig } from "vite";
 
 export default defineConfig({
-  base: "/business-static-site/",
   plugins: [react()],
   resolve: {
     alias: {
