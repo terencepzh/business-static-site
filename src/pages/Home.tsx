559ea65c0f814d21afb6515c76be60c8e1cd6dbc
lennon <lemoncode8888@gmail.com>
2024-10-16 18:09:09 +0800

    removed unused imports

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,53 +1,6 @@
 import { Button } from "@/components/ui/button";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-  CarouselNext,
-  CarouselPrevious,
-} from "@/components/ui/carousel";
-import {
-  Card,
-  CardContent,
-  CardDescription,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card";
-import Faq from "@/components/Faq";
-import About from "@/components/About";
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Link } from "react-router-dom";
-
-const FAQ = [
-  {
-    question: "Can you provide examples?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a tincidunt erat, a convallis lectus. Praesent iaculis lorem eu mollis.",
-  },
-  {
-    question: "Can you help?",
-    answer:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec a tincidunt erat, a convallis lectus. Praesent iaculis lorem eu mollis.",
-  },
-];
-
-const AboutContent = [
-  {
-    title: "Our Company",
-    background:
-      "bg-[url(https://media.istockphoto.com/id/1487894858/photo/candlestick-chart-and-data-of-financial-market.jpg?s=612x612&w=0&k=20&c=wZ6vVmbm4BV2JOePSnNNz-0aFVOJZ0P9nhdeOMGUg5I=)] bg-center",
-  },
-  {
-    title: "What we do",
-    background:
-      "bg-[url(https://t4.ftcdn.net/jpg/02/61/31/83/360_F_261318391_vCfeZxtPmq1tCXFbPuuX0GkzutiVJKM5.jpg)] bg-cover",
-  },
-  {
-    title: "Our locations",
-    background:
-      "bg-[url(https://upload.wikimedia.org/wikipedia/commons/thumb/1/1c/Raffles_Place_MRT_station_exit_A_221023.jpg/800px-Raffles_Place_MRT_station_exit_A_221023.jpg)] bg-center",
-  },
-];
 
 function App() {
   return (
@@ -148,61 +101,6 @@ function App() {
         </h3>
         <div>Testimonials TBD</div>
       </div>
-      {/* <div className="flex flex-col gap-12 items-center mx-4">
-        <div className="flex flex-col flex-wrap justify-center items-center gap-4 w-full">
-          <h1 className="text-2xl md:text-4xl font-bold pt-12">About Us</h1>
-          <About content={AboutContent} />
-        </div>
-        <div className="flex flex-col justify-center items-center gap-4 w-full">
-          <h1 className="text-2xl md:text-4xl font-bold">Testimonials</h1>
-          <Carousel
-            opts={{ align: "start", loop: true }}
-            className="w-[70%] max-w-sm sm:max-w-2xl xl:max-w-4xl"
-          >
-            <CarouselContent className="">
-              {Array.from({ length: 5 }).map((_, index) => (
-                <CarouselItem
-                  key={index}
-                  className="sm:basis-1/2 xl:basis-1/3"
-                >
-                  <div className="p-1">
-                    <Card>
-                      <CardContent className="flex aspect-square items-center justify-center p-6">
-                        <span className="text-4xl font-semibold">
-                          {index + 1}
-                        </span>
-                      </CardContent>
-                    </Card>
-                  </div>
-                </CarouselItem>
-              ))}
-            </CarouselContent>
-            <CarouselPrevious />
-            <CarouselNext />
-          </Carousel>
-        </div>
-        <div className="flex flex-col justify-center gap-4 max-w-xl">
-          <h1 className="text-2xl md:text-4xl font-bold text-center">
-            Our Services
-          </h1>
-          <p>
-            Navigating the PR application process in Singapore can be complex,
-            but you don't have to do it alone.
-          </p>
-          <p>
-            Our comprehensive suite of services at A1 Immigration Consultancy is
-            designed to cater to your specific needs. With our team of
-            immigration services experts, we're committed to guiding you through
-            each step, ensuring a smooth and successful application journey.
-          </p>
-        </div>
-        <div className="flex flex-col justify-center items-center gap-4 w-full max-w-xl">
-          <h1 className="text-2xl md:text-4xl font-bold text-center">
-            Frequently Asked Questions
-          </h1>
-          <Faq content={FAQ} />
-        </div>
-      </div> */}
       <div className="flex flex-col justify-center items-center gap-12 text-orange-500 text-left px-6 py-8">
         <div className="flex flex-col gap-4">
           <h1 className="text-4xl xl:text-5xl text-center font-bold">
